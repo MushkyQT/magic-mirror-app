5f9101beafeda7a3d723d703b803e4980ae4a140
Charles Melki <charlesmelki@gmail.com>
2020-11-26 15:37:01 +0100

    Change Mirror entity's ipAddress value to be nullable

--- src/Entity/Mirror.php
+++ src/Entity/Mirror.php
@@ -28,7 +28,7 @@ class Mirror
     private $isIdentified;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $ipAddress;
 
