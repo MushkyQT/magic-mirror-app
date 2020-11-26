95bbf71d6cf405037bd1cc496851b74abd95653a
Charles Melki <charlesmelki@gmail.com>
2020-11-26 11:21:43 +0100

    Generate user isVerified migration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -55,7 +55,7 @@ class RegistrationController extends AbstractController
             );
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('_profiler_home');
+            return $this->redirectToRoute('/');
         }
 
         return $this->render('registration/register.html.twig', [
@@ -79,9 +79,8 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('app_register');
         }
 
-        // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('/');
     }
 }
